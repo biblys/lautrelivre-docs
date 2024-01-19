5d521724f0d91e28b611e5321f912df837ef4c35
Clément Latzarus <c@ltzr.io>
2024-01-19 13:29:38 +0100

    feat: customize template

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,21 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+			title: 'L\'Autre Livre',
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'Français',
+					lang: 'fr',
+				},
+			},
+			editLink: {
+				baseUrl: 'https://github.com/biblys/lautrelivre-docs/edit/main/',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Administration',
+					autogenerate: { directory: 'admin' },
 				},
 			],
 		}),
